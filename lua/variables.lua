3ec4e53d0b8c453a3b9712947eaac2c371d1867e
Franco Serio <francoagustinserio@gmail.com>
2022-11-16 09:41:21 -0300

    setup nvim-tree for dotfiles and relativenumbers

--- lua/variables.lua
+++ lua/variables.lua
@@ -7,7 +7,13 @@ vim.g.loaded_netrwPlugin = 1
 
 vim.o.termguicolors = true
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+  filters = { dotfiles = false },
+  view = { 
+    relativenumber = true,
+    number = true 
+  }
+})
 
 -- TOKYONIGHT COLORSCHEME
 vim.cmd[[colorscheme tokyonight]]
