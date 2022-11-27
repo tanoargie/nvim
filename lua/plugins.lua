9eb154207ef030e093bc290a0b66816d770d0786
Franco Serio <francoagustinserio@gmail.com>
2022-11-26 21:25:08 -0300

    Add auto-format for supported lsp-servers

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -24,6 +24,7 @@ return require('packer').startup(function(use)
     use 'saadparwaiz1/cmp_luasnip'
     use 'L3MON4D3/LuaSnip'
     use 'mfussenegger/nvim-dap'
+    use 'lukas-reineke/lsp-format.nvim'
     use {
       "windwp/nvim-autopairs",
       config = function() require("nvim-autopairs").setup {} end
