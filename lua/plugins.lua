f76f04684125a80cc3e1fc78a3e71c1378fa4773
Franco Serio <francoagustinserio@gmail.com>
2022-12-31 02:10:23 -0300

    add vim-silicon to plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,33 +1,34 @@
 return require('packer').startup(function(use)
-    use {
-      'nvim-tree/nvim-tree.lua',
-      requires = {
-          'nvim-tree/nvim-web-devicons', -- optional, for file icons
-      },
-    }
-    use 'folke/tokyonight.nvim'
-    use {
-        'numToStr/Comment.nvim',
-        config = function()
-           require('Comment').setup()
-        end
-    }
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
-    use 'neovim/nvim-lspconfig'
-    use 'hrsh7th/nvim-cmp'
-    use 'tpope/vim-fugitive'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'saadparwaiz1/cmp_luasnip'
-    use 'L3MON4D3/LuaSnip'
-    use 'mfussenegger/nvim-dap'
-    use 'lukas-reineke/lsp-format.nvim'
-    use {
-      "windwp/nvim-autopairs",
-      config = function() require("nvim-autopairs").setup {} end
-    }
-    use 'vim-test/vim-test'
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    },
+  }
+  use 'folke/tokyonight.nvim'
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  }
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/nvim-cmp'
+  use 'tpope/vim-fugitive'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'L3MON4D3/LuaSnip'
+  use 'mfussenegger/nvim-dap'
+  use 'lukas-reineke/lsp-format.nvim'
+  use {
+    "windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+  }
+  use 'vim-test/vim-test'
+  use 'segeljakt/vim-silicon'
 end)
