9d1f244d30b11d0ba21ed0836e7c8cfeba74a427
Franco Serio <francoagustinserio@gmail.com>
2023-01-08 01:38:43 -0300

    added autoread in settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,15 +1,16 @@
 local settings = {
-    number = true,
-    relativenumber = true,
-    hidden = true,
-    clipboard = "unnamedplus",
-    autoindent = true,
-    smartindent = true,
-    expandtab = true,
-    tabstop = 2,
-    shiftwidth = 2
+  autoread = true,
+  number = true,
+  relativenumber = true,
+  hidden = true,
+  clipboard = "unnamedplus",
+  autoindent = true,
+  smartindent = true,
+  expandtab = true,
+  tabstop = 2,
+  shiftwidth = 2
 }
 
 for k, v in pairs(settings) do
-    vim.o[k] = v
+  vim.o[k] = v
 end
