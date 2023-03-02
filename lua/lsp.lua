6bf071f8a55a32b0f7f78787389eaa4040bb1922
Franco Serio <francoagustinserio@gmail.com>
2023-03-02 09:17:33 -0300

    replace sumneko_lua

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -31,7 +31,7 @@ local on_attach = function(client, bufnr)
 end
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'clangd', 'solargraph', 'tsserver', 'pyright', 'rust_analyzer', 'sumneko_lua', 'cmake', 'volar' }
+local servers = { 'clangd', 'solargraph', 'tsserver', 'pyright', 'rust_analyzer', 'lua_ls', 'cmake', 'volar' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     -- on_attach = my_custom_on_attach,
