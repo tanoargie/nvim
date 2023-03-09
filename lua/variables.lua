a9f0228cee06340979e91f25d036be640b3699d7
Franco Serio <franco@samser.co>
2023-03-09 19:43:47 -0300

    replace open_on_setup for nvim-tree

--- lua/variables.lua
+++ lua/variables.lua
@@ -7,10 +7,11 @@ vim.g.loaded_netrwPlugin = 1
 
 vim.o.termguicolors = true
 
+-- NVIM-TREE
+
 local nvimTree = require('nvim-tree')
 
 require("nvim-tree").setup({
-  open_on_setup = true,
   filters = { dotfiles = false },
   actions = {
     open_file = {
@@ -25,6 +26,30 @@ require("nvim-tree").setup({
     enable = true
   }
 })
+
+-- open_on_setup replacement
+local function open_nvim_tree(data)
+
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not no_name and not directory then
+    return
+  end
+
+  -- change to the directory
+  if directory then
+    vim.cmd.cd(data.file)
+  end
+
+  -- open the tree
+  require('nvim-tree.api').tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
 -- NVIM-TREESITTER
 
