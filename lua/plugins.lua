61895d3017226379dbac5347cadad0e6f97e77e5
Franco Serio <franco@samser.co>
2026-05-08 01:43:01 +0200

    feat: remove copilot plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -31,7 +31,6 @@ return require('packer').startup(function(use)
   }
   use 'neovim/nvim-lspconfig'
   use 'hrsh7th/nvim-cmp'
-  use 'github/copilot.vim'
   use 'tpope/vim-fugitive'
   use 'hrsh7th/cmp-nvim-lsp'
   use 'SirVer/ultisnips'
